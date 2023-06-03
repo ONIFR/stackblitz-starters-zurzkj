7ccf4a01a9ca154699fe5082b70ae7a5e6aca630
ONIFR <50652879+ONIFR@users.noreply.github.com>
2023-06-03 20:48:35 +0200

    1 file modified

--- pages/team.js
+++ pages/team.js
@@ -93,19 +93,19 @@ const TeamPage = () => {
       <div id="team-selection-container">
         <div className="team-container" id="frogz-container">
           <div className="team-title">The Frogz Team</div>
-          <img src="https://cdn.discordapp.com/attachments/1090010869343195188/1114087970090266654/SamPlayer20_a_3D_cube_with_Pepee_the_frog_on_each_surface_of_th_946758" alt="Frogz" className="team-image" id="frogz-image" />
+          <img src="https://cdn.discordapp.com/attachments/1090010869343195188/1114087970090266654/SamPlayer20_a_3D_cube_with_Pepee_the_frog_on_each_surface_of_th_946758.png" alt="Frogz" className="team-image" id="frogz-image" />
           <br />
           <button className="team-button" onClick={() => confirmTeamSelection('Frogz')}>Join the team</button>
         </div>
         <div className="team-container" id="monkz-container">
           <div className="team-title">The Monkz Team</div>
-          <img src="https://cdn.discordapp.com/attachments/1090010869343195188/1113799982785499377/SamPlayer20_a_3D_cube_with_a_Bored_Ape_on_eatch_surface_of_the__76f515cf-6fc6-47e2-9a19-0b6e33bc8f16" alt="Monkz" className="team-image" id="monkz-image" />
+          <img src="https://cdn.discordapp.com/attachments/1090010869343195188/1113799982785499377/SamPlayer20_a_3D_cube_with_a_Bored_Ape_on_eatch_surface_of_the__76f515cf-6fc6-47e2-9a19-0b6e33bc8f16.png" alt="Monkz" className="team-image" id="monkz-image" />
           <br />
           <button className="team-button" onClick={() => confirmTeamSelection('Monkz')}>Join the team</button>
         </div>
         <div className="team-container" id="shibz-container">
           <div className="team-title">The Shibz Team</div>
-          <img src="https://cdn.discordapp.com/attachments/1090010869343195188/1114087621426151434/SamPlayer20_a_3D_cube_with_a_cartoon_Shiba_on_each_surface_of_t_bda40775-fb8c-40fd-b5f1-e7b808a3b6b8" alt="Shibz" className="team-image" id="shibz-image" />
+          <img src="https://cdn.discordapp.com/attachments/1090010869343195188/1114087621426151434/SamPlayer20_a_3D_cube_with_a_cartoon_Shiba_on_each_surface_of_t_bda40775-fb8c-40fd-b5f1-e7b808a3b6b8.png" alt="Shibz" className="team-image" id="shibz-image" />
           <br />
           <button className="team-button" onClick={() => confirmTeamSelection('Shibz')}>Join the team</button>
         </div>
